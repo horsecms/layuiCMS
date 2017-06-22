c14a467525e293054d631156c300624a4183988f
BrotherMa <ming13811833840@vip.qq.com>
2017-06-22 11:02:20 +0800

    Add files via upload

---
+++ nav.js
@@ -0,0 +1,45 @@
+var navs = [{
+	"title" : "后台首页",
+	"icon" : "icon-computer",
+	"href" : "page/main.html",
+	"spread" : false
+},{
+	"title" : "文章列表",
+	"icon" : "icon-text",
+	"href" : "page/news/newsList.html",
+	"spread" : false
+},{
+	"title" : "友情链接",
+	"icon" : "icon-text",
+	"href" : "page/links/linksList.html",
+	"spread" : false
+},{
+	"title" : "404页面",
+	"icon" : "&#xe61c;",
+	"href" : "page/404.html",
+	"spread" : false
+},{
+	"title" : "系统基本参数",
+	"icon" : "&#xe631;",
+	"href" : "page/systemParameter/systemParameter.html",
+	"spread" : false
+},{
+	"title" : "二级菜单演示",
+	"icon" : "&#xe61c;",
+	"href" : "",
+	"spread" : false,
+	"children" : [
+		{
+			"title" : "二级菜单1",
+			"icon" : "&#xe631;",
+			"href" : "",
+			"spread" : false
+		},
+		{
+			"title" : "二级菜单2",
+			"icon" : "&#xe631;",
+			"href" : "",
+			"spread" : false
+		}
+	]
+}]
