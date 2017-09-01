139aeea045b80ef1a06c6d098c6264db1ec0cf97
BrotherMa <ming13811833840@vip.qq.com>
2017-09-01 11:24:55 +0800

    Update bodyTab.js

--- js/bodyTab.js
+++ js/bodyTab.js
@@ -73,7 +73,7 @@ layui.define(["element","jquery"],function(exports){
 			tabFilter = that.tabConfig.tabFilter;
 		if(_this.attr("target") == "_blank"){
 			window.location.href = _this.attr("data-url");
-		}else{
+		}else if(_this.attr("data-url") != undefined){
 			var title = '';
 			if(_this.find("i.iconfont,i.layui-icon").attr("data-icon") != undefined){
 				if(_this.find("i.iconfont").attr("data-icon") != undefined){
@@ -83,7 +83,7 @@ layui.define(["element","jquery"],function(exports){
 				}
 			}
 			//已打开的窗口中不存在
-			if(that.hasTab(_this.find("cite").text()) == -1 && _this.siblings("dl.layui-nav-child").length == 0 && _this.attr("data-url")){
+			if(that.hasTab(_this.find("cite").text()) == -1 && _this.siblings("dl.layui-nav-child").length == 0){
 				if($(".layui-tab-title.top_tab li").length == openTabNum){
 					layer.msg('只能同时打开'+openTabNum+'个选项卡哦。不然系统会卡的！');
 					return;
